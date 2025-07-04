c99db6c95a380fa81e32c46b0dd03ce2c9142fd4
Juan Gómez <gb.jc@outlook.com>
2025-07-04 16:44:15 -0400

    Added initial migration and handler for connection to SQL DB also admin user inserted in table.

---
+++ Microservices/Services.API.Security/Core/Persistence/SecurityData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Services.API.Security.Core.Entities;
+
+namespace Services.API.Security.Core.Persistence
+{
+  public class SecurityData
+  {
+    public static async Task SaveInitialUser(SecurityContext securityContext, UserManager<User> userManager) {
+      if (!userManager.Users.Any())
+      {
+        var user = new User
+        {
+          Name = "Admin",
+          LastName = "Admin",
+          Address = "123 Admin St",
+          UserName = "admin",
+          Email = "admin@fake.com"
+        };
+
+        await userManager.CreateAsync(user, "MySuperPassword123!");
+      } 
+    }
+  }
+}
--- Microservices/Services.API.Security/Program.cs
+++ Microservices/Services.API.Security/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Services.API.Security.Core.Entities;
+using Services.API.Security.Core.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SecurityContext>(options => { 
+  options.UseSqlServer(builder.Configuration.GetConnectionString("SecurityDB"));
+});
+
+builder.Services
+  .AddIdentityCore<User>()
+  .AddEntityFrameworkStores<SecurityContext>()
+  .AddSignInManager<SignInManager<User>>();
+
+builder.Services.AddSingleton<TimeProvider>(TimeProvider.System);
 
 var app = builder.Build();
 
@@ -20,4 +38,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await SeedDatabaseAsync(app);
+
 app.Run();
+
+async Task SeedDatabaseAsync(WebApplication app)
+{
+  using var scope = app.Services.CreateScope();
+  var services = scope.ServiceProvider;
+  try
+  {
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var securityDbContext = services.GetRequiredService<SecurityContext>();
+    await SecurityData.SaveInitialUser(securityDbContext, userManager);
+  }
+  catch (Exception e)
+  {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred while seeding the database.");
+  }
+}
