5beba5b020c73fc98eecfb839c788f932fe0439c
Juan Gómez <gb.jc@outlook.com>
2025-07-14 17:15:29 -0400

    Fixed ocelot settings.

--- Microservices/Services.API.Gateway/Program.cs
+++ Microservices/Services.API.Gateway/Program.cs
@@ -1,16 +1,32 @@
-using Ocelot.DependencyInjection;
+using System.Text;
 using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+var jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = jwtKey,
+      ValidateAudience = false,
+      ValidateIssuer = false,
+    };
+  });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
