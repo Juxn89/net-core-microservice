b8cec6149470ca8cd419eb8f37ac7e9b8f81a2dd
Juan Gómez <gb.jc@outlook.com>
2025-07-15 17:50:39 -0400

    Connect SecurityServices to backend

--- client/src/app/security/security.service.ts
+++ client/src/app/security/security.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router'
 import { Subject } from 'rxjs';
 
 import { User } from "./user.model";
 import { LoginData } from "./login-data.model";
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class SecurityService {
+  baseURL = environment.baseUrl;
+  private httpClient = inject(HttpClient)
+
   private user!: User;
   private isLogIn = new Subject<boolean>();
 
   isLogIn$ = this.isLogIn.asObservable()
 
-  constructor(private router: Router){}
+  constructor(
+    private router: Router
+  ){}
 
   register(newUser: User) {
     this.user = {
@@ -26,6 +33,11 @@ export class SecurityService {
   }
 
   login(loginData: LoginData) {
+    this.httpClient.post(`${this.baseURL}user/login`, loginData)
+      .subscribe(response => {
+        console.log(response)
+      })
+
     this.user = {
       ...this.user,
       email: loginData.email,
@@ -35,7 +47,7 @@ export class SecurityService {
 
     this.isLogIn.next(true)
 
-    this.router.navigate(['/'])
+    // this.router.navigate(['/'])
   }
 
   clearSession() {
